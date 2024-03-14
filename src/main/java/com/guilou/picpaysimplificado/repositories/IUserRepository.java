ed9cd3d78c4b1846eb06b317ffdaf472b3678904
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2024-03-14 14:25:52 -0300

    create IUserRepository

--- src/main/java/com/guilou/picpaysimplificado/repositories/IUserRepository.java
+++ src/main/java/com/guilou/picpaysimplificado/repositories/IUserRepository.java
@@ -1,12 +1,14 @@
 package com.guilou.picpaysimplificado.repositories;
 
-import com.guilou.picpaysimplificado.models.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.guilou.picpaysimplificado.models.User.User;
+
 public interface IUserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByDocument(String document);
+
     Optional<User> findUserById(UUID id);
 }
